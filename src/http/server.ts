b411e4f2797f545f4b5b53dfe8873ffa3be26a98
Octavio Duarte <octavioduarte2105@gmail.com>
2021-03-12 14:44:07 -0300

    refactor: create file with env params

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,7 @@
-import express from 'express'
 import { appHttp } from './config/app'
+import { env } from '../env'
 
 
-appHttp.server.listen(8000)
+appHttp.server.listen(env.port, () => {
+    console.info(`Server HTTP running at port: ${env.port}`)
+})
